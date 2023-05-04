87ac97241acbccb4e7a6247e711dd77cefaa823f
bd26 <shbadhon10@gmail.com>
2023-05-05 00:08:30 +0600

    okay

--- lua/bd26/init.lua
+++ lua/bd26/init.lua
@@ -1,8 +1,3 @@
 dofile(vim.g.binary .. "statusline")
 vim.opt.statusline = "%!v:lua.require('bd26.statusline').run()"
-
-vim.defer_fn(function()
-  require("bd26.dash").open()
-end, 0)
-
 require("bd26.customcommand")
