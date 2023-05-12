7ed1f772861d852595951862d5485fbf3a685388
Badhon <shbadhon10@gmail.com>
2023-05-12 21:40:47 +0600

    fixed

--- lua/bd26/init.lua
+++ lua/bd26/init.lua
@@ -1,9 +1,10 @@
 vim.defer_fn(function()
   require("bd26.dash").open()
-  vim.cmd("set nobuflisted")
+  -- vim.cmd("set nobuflisted")
 end, 0)
+
+require("bd26.tabufline.lazyload")
 
 dofile(vim.g.binary .. "statusline")
 vim.opt.statusline = "%!v:lua.require('bd26.statusline').run()"
 require("bd26.customcommand")
-require("bd26.tabufline.lazyload")
