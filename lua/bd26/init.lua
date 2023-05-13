a81212113bab4c861e37131c72d98d4c26740396
Badhon <shbadhon10@gmail.com>
2023-05-13 22:02:42 +0600

    added mappings

--- lua/bd26/init.lua
+++ lua/bd26/init.lua
@@ -8,3 +8,12 @@ require("bd26.tabufline.lazyload")
 dofile(vim.g.binary .. "statusline")
 vim.opt.statusline = "%!v:lua.require('bd26.statusline').run()"
 require("bd26.customcommand")
+
+local kcmd = vim.api.nvim_create_user_command
+kcmd("ShowMappings", function()
+  if vim.g.nvcheatsheet_displayed then
+    vim.cmd "bd"
+  else
+    require("bd26.cheatsheet.grid")()
+  end
+end, {})
